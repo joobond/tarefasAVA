e276235cca1d56851f50cc7544e7b7a5cad5ff9b
Joao Bond <jobond46@gmail.com>
2019-03-15 21:52:33 -0400

    Ate 6.6

--- app/src/main/java/br/edu/ifro/vilhena/ads/tarefas/ListarTarefasActivity.java
+++ app/src/main/java/br/edu/ifro/vilhena/ads/tarefas/ListarTarefasActivity.java
@@ -1,19 +1,26 @@
 package br.edu.ifro.vilhena.ads.tarefas;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import br.edu.ifro.vilhena.ads.tarefas.DAO.AppDatabase;
+import br.edu.ifro.vilhena.ads.tarefas.adapter.ListarTarefasAdapter;
 import br.edu.ifro.vilhena.ads.tarefas.model.Tarefa;
 
 public class ListarTarefasActivity extends AppCompatActivity {
 
     private ListView lsvListarTarefas;
     private List<Tarefa> tarefas;
+    private FloatingActionButton fabCadastrarTarefa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +28,30 @@ public class ListarTarefasActivity extends AppCompatActivity {
         setContentView(R.layout.activity_listar_tarefas);
 
         lsvListarTarefas = (ListView) findViewById(R.id.lsv_listar_tarefas);
+        fabCadastrarTarefa = (FloatingActionButton) findViewById(R.id.fab_cadastrar_tarefa);
 
-        tarefas =  new ArrayList<Tarefa>();
+        fabCadastrarTarefa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListarTarefasActivity.this, CadastrarTarefaActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
 
-        Tarefa t1 = new Tarefa(1, "Pagar boleto de energia");
-        Tarefa t2 = new Tarefa(2, "Estudar para a prova de matemática");
-        Tarefa t3 = new Tarefa(3, "Compra biscoito");
-
-        tarefas.add(t1);
-        tarefas.add(t2);
-        tarefas.add(t3);
-
-        ArrayAdapter<Tarefa> adapter = new ArrayAdapter<Tarefa>(this, android.R.layout.simple_list_item_1, tarefas);
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        tarefas = AppDatabase.getAppDatabase(this).tarefaDAO().listarTodos();
+        ListarTarefasAdapter adapter = new ListarTarefasAdapter(tarefas, this);
         lsvListarTarefas.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 1){
+            Snackbar.make(findViewById(R.id.layout_listar_tarefas), "Tarefa cadastrada com sucesso!", Snackbar.LENGTH_LONG).show();
+        }
     }
 }
